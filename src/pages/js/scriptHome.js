6821c15be42383b25dcdc48eb1036aca3790604b
Otávio Albuquerque <otavioalbe@gmail.com>
2024-03-21 04:00:51 -0300

    final

--- src/pages/js/scriptHome.js
+++ src/pages/js/scriptHome.js
@@ -25,9 +25,8 @@ botaoSair.addEventListener('click', function goBack() {
     if(confirm('Deseja sair? As apostas serão redefinidas.')){
         fetch('http://localhost:8080/sorteio/reiniciar-aposta')
         .then(response => response.json())
-        .then(apostas => {
-            consoleg.log(apostas)
-        }).catch(error => console.log(error))
+        .then(apostas => {consoleg.log(apostas)})
+        .catch(error => console.log(error))
 
         localStorage.setItem('apostaAberta', true);
         location.href = 'index.html'
