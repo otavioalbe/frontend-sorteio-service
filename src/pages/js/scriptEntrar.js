e9e77e5d773c3dc2c819d68fa78e0d8cc9dca35a
Otávio Albuquerque <otavioalbe@gmail.com>
2024-03-27 14:47:20 -0300

    correção apostas fechadas

--- src/pages/js/scriptEntrar.js
+++ src/pages/js/scriptEntrar.js
@@ -1,5 +1,6 @@
 let botaoEntrar = document.getElementById('btnEntrar')
 
 botaoEntrar.addEventListener('click', function Entrar() {
+    localStorage.setItem('apostaAberta', true);
     location.href = 'home.html'
 })
